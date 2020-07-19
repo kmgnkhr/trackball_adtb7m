8c79164f8877885d0d4883127aa39010e9ded5e7
kmgnkhr <49336378+kmgnkhr@users.noreply.github.com>
2020-07-19 10:04:38 +0900

    Add port access functions

--- trackball_adtb7m/trackball_adtb7m.ino
+++ trackball_adtb7m/trackball_adtb7m.ino
@@ -15,51 +15,56 @@ void Pulse() {
   ::delayMicroseconds(5);
 }
 
-void SCLK_Direction(uint8_t mode) {
+void Direction(uint8_t mode, uint8_t bit,
+               volatile uint8_t* reg, volatile uint8_t* out) {
   switch (mode) {
     case OUTPUT:
-      DDRB |= _BV(1);
+      *reg |= bit;
       break;
     case INPUT:
-      // DDRB &= ~_BV(1);
-      // PORTB &= ~_BV(1);
+      *reg &= ~bit;
+      *out &= ~bit;
+      break;
+    case INPUT_PULLUP:
+      *reg &= ~bit;
+      *out |= bit;
       break;
   }
 }
 
-void SCLK(uint8_t val, bool wait = true) {
+void WritePort(uint8_t val, uint8_t bit, volatile uint8_t* reg) {
   if (val == LOW) {
-    PORTB &= ~_BV(1);
+    *reg &= ~bit;
   } else {
-    PORTB |= _BV(1);
+    *reg |= bit;
   }
+}
+
+bool ReadPort(uint8_t bit, volatile const uint8_t* reg) {
+  return (*reg & bit);
+}
+
+void SCLK_Direction(uint8_t mode) {
+  Direction(mode, _BV(1), &DDRB, &PORTB);
+}
+
+void SCLK(uint8_t val, bool wait = true) {
+  WritePort(val, _BV(1), &PORTB);
   if (wait) {
     Pulse();
   }
 }
 
 void SDIO_Direction(uint8_t mode) {
-  switch (mode) {
-    case OUTPUT:
-      DDRB |= _BV(3);
-      break;
-    case INPUT:
-      DDRB &= ~_BV(3);
-      PORTB &= ~_BV(3);
-      break;
-  }
+  Direction(mode, _BV(3), &DDRB, &PORTB);
 }
 
 void SDIO(uint8_t val) {
-  if (val == LOW) {
-    PORTB &= ~_BV(3);
-  } else {
-    PORTB |= _BV(3);
-  }
+  WritePort(val, _BV(3), &PORTB);
 }
 
-uint8_t SDIO() {
-  return (PINB & _BV(3)) ? HIGH : LOW;
+bool SDIO() {
+  return ReadPort(_BV(3), &PINB);
 }
 
 void SetupSerial() {
@@ -87,7 +92,7 @@ uint8_t ReadRegister(int8_t address) {
   auto rx = 0;
   for (auto b = 0x80; b != 0; b >>= 1) {
     SCLK(LOW);
-    if (SDIO() == HIGH) {
+    if (SDIO()) {
       rx |= b;
     }
     SCLK(HIGH);
